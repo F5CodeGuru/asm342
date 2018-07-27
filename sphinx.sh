357451012358a239d9926b3751d280d9670b3812
below-average-f5-admin <rj@f5.com>
2018-07-27 13:01:27 -0400

    Module2 clean up

--- sphinx.sh
+++ sphinx.sh
@@ -1,2 +1,2 @@
-sphinx-autobuild -E . ./_build/html/ -H 0.0.0.0
-#sphinx-autobuild . ./_build/html/ -H 0.0.0.0
+#sphinx-autobuild -E . ./_build/html/ -H 0.0.0.0
+sphinx-autobuild . ./_build/html/ -H 0.0.0.0
